985aa1aafebbdc999f48e232e168208f153f4e58
KURAPIKA <KURAPIKA@DESKTOP-0HOQ138>
2025-02-02 11:59:01 +0100

    Add project files.

---
+++ GestionSubcription/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionSubcription.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
